9b19dfaeb132bbf54ed76506ae2afd95b453a1fc
Moary Chen <v-moaryc@microsoft.com>
2021-11-01 09:02:13 +0800

    Revert the setting of the variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "application_name" {
   type        = string
   description = "The name of your application"
-  default     = "moaryc-sample"
+  default     = "<your-alias>-sample"
 }
 
 variable "environment" {
   type        = string
   description = "The environment (dev, test, prod...)"
-  default     = "dat"
+  default     = ""
 }
 
 variable "location" {
   type        = string
   description = "The Azure region where all resources in this example should be created"
-  default     = "eastasia"
+  default     = "eastus"
 }
 
 variable "service_principal_id" {
   type        = string
   description = "The Azure Service Principal object_id to assign role to, not the application id here"
-  default     = "0dfd3115-09fb-442f-9798-c91a139a53be"
+  default     = ""
 }
